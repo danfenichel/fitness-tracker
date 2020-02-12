314c1c752900a5dfd666d8a9d1df40403ed9cba2
D0491 <dan.fenichel@yahoo.com>
2020-02-11 18:53:12 -0600

    Set up initial files

---
+++ models/Workout.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    type: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: false
+    },
+    reps: {
+        type: Number,
+    }
+})
