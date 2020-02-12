cf49816f8e35bc1e089c9100c851c96eadd04743
D0491 <dan.fenichel@yahoo.com>
2020-02-12 10:41:28 -0600

    fixed models

--- models/Workout.js
+++ models/Workout.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-    type: {
-        type: String,
-        required: true
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
     },
-    name: {
-        type: String,
-        required: true
-    },
-    duration: {
-        type: Number,
-        required: true
-    },
-    weight: {
-        type: Number,
-        required: false
-    },
-    reps: {
-        type: Number,
-    }
-})
+    exercises: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Exercise"
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
